c05af8b1137daa31db193fe56870ee5ef19da22d
ariia horunzha <1mpellee@gmail.com>
2024-11-12 18:02:20 +0200

    first commit

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
-
+const array = () => {
+    const data = [];
+    const get = (index) => data[index];
+    get.pop = () => data.pop();
+    get.push = (el) => data.push(el);
+    return get;
+};
+  
 module.exports = { array };
